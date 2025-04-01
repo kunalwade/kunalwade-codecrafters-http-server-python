a7a5f64f89d9beec47799f4314e79220404f8685
Kunal <wadekunal1234@gmail.com>
2025-04-01 18:10:23 +0530

    extract url path

--- app/main.py
+++ app/main.py
@@ -7,8 +7,13 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     # server_socket.accept() # wait for client
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
-
+    server_socket.listen()
+    client, addr = server_socket.accept()
+    client_msg = client.recv(4096).decode().split(" ")
+    if client_msg[1] == "/":
+        client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 if __name__ == "__main__":
     main()
