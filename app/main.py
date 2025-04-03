15e306071aeac714b2c0f5578e99b42f5e7438fe
Kunal <wadekunal1234@gmail.com>
2025-04-03 09:57:28 +0530

    Refactor code

--- app/main.py
+++ app/main.py
@@ -1,163 +1,25 @@
-import sys
-import socket
-import threading
-
-# Config
-HOST = "localhost"
-PORT = 4221
-FILES_DIR = ""
-
-# HTTP Statuses
-HTTP_STATUS_OK = "HTTP/1.1 200 OK\r\n"
-HTTP_STATUS_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n"
-HTTP_STATUS_CREATED = "HTTP/1.1 201 Created\r\n"
+from app.handler import HTTPHandler
+from app.server import HTTPServer
+import argparse
 
 
-def response(status, headers, body):
-    """
-    Returns a properly formatted HTTP response.
+def main() -> None:
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--directory')
 
-    Args:
-        status (str): The HTTP status code and message.
-        headers (dict): A dictionary of HTTP headers.
-        body (str): The response body.
+    args = arg_parser.parse_args()
 
-    Returns:
-        bytes: The encoded HTTP response.
-    """
-    return (status +
-            "".join(f"{k}: {v}\r\n" for k, v in headers.items()) +
-            "\r\n" +
-            body + "\r\n").encode()
+    config = {}
 
+    if args.directory:
+        config['directory'] = args.directory
 
-def handle_request(request, files=None):
-    """
-    Handle incoming HTTP requests and generate appropriate HTTP responses.
-
-    Args:
-        request (list): A list containing the HTTP request data.
-        files (str, optional): The path to the directory containing
-                               files to be served. Defaults to None.
-
-    Returns:
-        response: An HTTP response object containing
-        the appropriate status code, headers, and body.
-    """
-    method, path, _ = request[0].split(" ")
-
-    if method == "GET" and path == "/":
-        res = response(
-            status=HTTP_STATUS_OK + "\r\n",
-            headers={},
-            body=""
-        )
-
-    elif method == "GET" and path.startswith('/echo'):
-        content = path.split("/echo/")[1]
-        res = response(
-            status=HTTP_STATUS_OK,
-            headers={"Content-Type": "text/plain",
-                     "Content-Length": len(content)},
-            body=content
-        )
-
-    elif method == "GET" and path.startswith('/user-agent'):
-        content = request[2].split(": ")[1]
-        res = response(
-            status=HTTP_STATUS_OK,
-            headers={"Content-Type": "text/plain",
-                     "Content-Length": len(content)},
-            body=content
-        )
-
-    elif path.startswith('/files'):
-        file_name = path.split("/files/")[1]
-        if method == "POST":
-            content = request[-1]
-            handle_files(
-                file_name=file_name,
-                file_content=content, mode="write")
-
-            res = response(
-                status=HTTP_STATUS_CREATED,
-                headers={"Content-Type": "text/plain",
-                         "Content-Length": len(content)},
-                body=content
-            )
-        else:
-            file_content = handle_files(
-                file_name=file_name, mode="read")
-            if file_content:
-                res = response(
-                    status=HTTP_STATUS_OK,
-                    headers={"Content-Type": "application/octet-stream",
-                             "Content-Length": len(file_content)},
-                    body=file_content
-                )
-            else:
-                res = response(
-                    status=HTTP_STATUS_NOT_FOUND + "\r\n",
-                    headers={},
-                    body=""
-                )
-
-    else:
-        res = response(
-            status=HTTP_STATUS_NOT_FOUND + "\r\n",
-            headers={},
-            body=""
-        )
-
-    return res
-
-
-def handle_files(file_name, file_content=None, mode="read"):
-    if mode == 'write':
-        with open(f"{FILES_DIR}{file_name}", "w", encoding='UTF8') as f:
-            file = f.write(file_content)
-
-    else:
+    with HTTPServer(('127.0.0.1', 4221), HTTPHandler, config=config) as server:
         try:
-            with open(f"{FILES_DIR}{file_name}", "r") as f:
-                file = f.read()
-        except FileNotFoundError:
-            file = None
-
-    return file
+            server.serve_forever()
+        except KeyboardInterrupt:
+            pass
 
 
-def handle_client(conn, addr):
-    """
-    Handle incoming client requests.
-
-    Args:
-        conn (socket): The client socket connection.
-        addr (tuple): The client address.
-
-    Returns:
-        bytes: The response to send back to the client.
-    """
-    request = conn.recv(4096).decode(
-        ).splitlines()
-
-    conn.send(handle_request(request))
-    conn.close()
-
-
-def main(args):
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    if args:
-        global FILES_DIR
-        FILES_DIR = args[1]
-
-    while True:
-        conn, addr = server_socket.accept() # wait for client
-
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-
-        thread.start()
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+if __name__ == '__main__':
+    main()
--- app/server.py
+++ app/server.py
@@ -1,35 +1,16 @@
-# import threading
-# import socket
+from socketserver import ThreadingTCPServer
+from typing import Dict, Optional
 
 
-# class TCPServer:
-#     def __init__(self, host: str, port: int) -> None:
-#         self.host = host
-#         self.port = port
-#         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class HTTPServer(ThreadingTCPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+    config: Dict
 
-#     def start_server(self) -> None:
-#         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#         self.server_socket.bind((self.host, self.port))
-#         self.server_socket.listen(3)
-#         print(f"Server started on {self.host}:{self.port}")
-#         while True:
-#             client_socket, client_address = self.server_socket.accept()
-#             print(f"Connection from {client_address}")
-#             threading.Thread(target=self.handle_request, args=(client_socket,)).start()
+    def __init__(self, *args, config: Optional[Dict] = None, **kvargs):
+        super().__init__(*args, **kvargs)
 
-#     def handle_request(self, client_socket: socket.socket) -> None:
-#         data = client_socket.recv(1024).decode("utf-8")
-#         lines = data.split("\r\n")
-#         path = lines[0].split()[1]
-#         if path == "/":
-#             response = "HTTP/1.1 200 OK\r\n\r\n"
-#         elif path.startswith("/echo/"):
-#             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
-#         elif path.startswith("/user-agent"):
-#             user_agent = lines[2].split(": ")[1]
-#             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-#         else:
-#             response = "HTTP/1.1 404 Not Found\r\n\r\n"
-#         client_socket.send(response.encode("utf-8"))
-#         client_socket.close()
+        self.config = {}
+
+        if config:
+            self.config.update(config)
